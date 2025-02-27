e41198a504872461777bfe8b50f591ff959288d0
AMIRSTAR1382 <144606664+AMIRSTAR1382@users.noreply.github.com>
2025-02-27 17:44:14 +0330

    change 5

---
+++ script/script.js
@@ -0,0 +1,20 @@
+function calculate() {
+                const amount = parseFloat(document.getElementById('amount').value);
+                const term = parseFloat(document.getElementById('term').value);
+                const interest = parseFloat(document.getElementById('interest').value) / 100 / 12;
+
+                const mortgageType = document.querySelector('input[name="mortgageType"]:checked').value;
+
+                    let monthlypayment,totalpayment;
+
+                    if (mortgageType === 'repayment') { monthlypayment = (amount * interestrate) / (1 - math.pow(1 + interestrate, -term * 12));
+                        totalpayment = monthlypayment * term * 12;
+                    } else if (mortgageType === 'interest-only') {
+                        monthlypayment = amount * interestrate;
+                        totalpayment = monthlypayment * term * 12;
+                    }
+
+                document.getElementById('monthly-payment').innerText = '₤${monthlypayment.toFixed(2)}';
+                document.getElementById('total-payment').innerText = '₤${totalpayment.toFixed(2)}';
+                document.getElementById('result').style.display = 'block';
+            }
