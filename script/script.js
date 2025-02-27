73a32e6351f08c72b00db3dc0abac005bf9426fd
AMIRSTAR1382 <144606664+AMIRSTAR1382@users.noreply.github.com>
2025-02-27 17:59:53 +0330

    new

--- script/script.js
+++ script/script.js
@@ -7,6 +7,10 @@ function calculate() {
 
                     let monthlypayment,totalpayment;
 
+                    if (isNaN(amount) || isNaN(term) || isNaN(interestrate) || amount <= 0 || term <= 0 || interestrate < 0){
+                        alert('Please enter valid values for all fields.');
+                    }
+
                     if (mortgageType === 'repayment') { monthlypayment = (amount * interestrate) / (1 - math.pow(1 + interestrate, -term * 12));
                         totalpayment = monthlypayment * term * 12;
                     } else if (mortgageType === 'interest-only') {
